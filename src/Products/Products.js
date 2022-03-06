e2117c3b806571ba082ba3e1c9c92d636d6c7a97
Deak Zsolt <dzsolti1293@gmail.com>
2022-03-06 12:42:42 +0200

    add product to cart

--- src/Products/Products.js
+++ src/Products/Products.js
@@ -7,7 +7,7 @@ function Products() {
       <h1>Products</h1>
 
       {products.map((product) => (
-      <Link to={`/products/${product.id}`} key={product.id}>
+      <Link to={`/products/${product.id}`} key={product.id} style={{ display: "block" }}>
           <img src={product.images[0]} width="200px" alt="product" />
           <h3>{product.title}</h3>
           <p>{product.description}</p>
